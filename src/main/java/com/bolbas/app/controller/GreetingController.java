ae01b6d4d71c101e7820242bbd9a118eda5d19bb
Artyom Bolbas <artyombolbas@yandex.ru>
2019-10-26 22:33:34 +0300

    Lesson 1. Serving Web Content with Spring MVC

--- src/main/java/com/bolbas/app/controller/GreetingController.java
+++ src/main/java/com/bolbas/app/controller/GreetingController.java
@@ -1,7 +1,8 @@
 package com.bolbas.app.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,9 +11,15 @@ public class GreetingController {
 
 	@GetMapping("/greeting")
 	public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
-			Model model) {
-		model.addAttribute("name", name);
+			Map<String, Object> model) {
+		model.put("name", name);
 		return "greeting";
+	}
+	
+	@GetMapping
+	public String main(Map<String, Object> model) {
+		model.put("some", "hello let's Code");
+		return "main";
 	}
 
 }
